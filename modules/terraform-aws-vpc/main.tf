eed9768dd8c0f2033109186957011767d9a56bc6
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-09 11:32:20 +0530

    fix

--- modules/terraform-aws-vpc/main.tf
+++ modules/terraform-aws-vpc/main.tf
@@ -54,7 +54,7 @@ resource "aws_route_table" "public" {
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  nat_gateway_id = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "public" {
