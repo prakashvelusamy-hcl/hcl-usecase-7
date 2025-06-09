3d6646a6ee018df29a8aabefd5741134c7045091
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-09 15:37:44 +0530

    fix

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"  # Pin to the latest 4.x version, or change to a specific version like "4.10.0"
+      version = "~> 5.0"
     }
   }
 }
