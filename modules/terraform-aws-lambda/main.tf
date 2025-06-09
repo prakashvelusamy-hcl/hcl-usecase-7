ef1174d235b30baadd6bc73f42631aec1c526d6c
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-09 11:39:00 +0530

    fix

--- modules/terraform-aws-lambda/main.tf
+++ modules/terraform-aws-lambda/main.tf
@@ -63,7 +63,7 @@ resource "aws_lambda_function" "docker_lambda" {
 
   package_type = "Image"
 
-  image_uri = "495599733393.dkr.ecr.ap-south-1.amazonaws.com/my-app:latest"
+  image_uri = "495599733393.dkr.ecr.ap-south-1.amazonaws.com/my-app:v1"
 
   role = aws_iam_role.lambda_exec.arn
 
